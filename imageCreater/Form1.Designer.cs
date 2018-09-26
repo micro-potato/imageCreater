22d26e34f31e340c8f3d8b414d1646a37995e603
micro-potato <micro_potato@hotmail.com>
2018-09-26 20:46:00 +0800

    Initial commit

---
+++ imageCreater/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace imageCreater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtYcount = new System.Windows.Forms.TextBox();
+            this.txtXCount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 248);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Width:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Height:";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(197, 45);
+            this.txtWidth.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(132, 25);
+            this.txtWidth.TabIndex = 3;
+            this.txtWidth.Text = "90";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(197, 82);
+            this.txtHeight.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(132, 25);
+            this.txtHeight.TabIndex = 4;
+            this.txtHeight.Text = "91";
+            // 
+            // txtYcount
+            // 
+            this.txtYcount.Location = new System.Drawing.Point(197, 161);
+            this.txtYcount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtYcount.Name = "txtYcount";
+            this.txtYcount.Size = new System.Drawing.Size(132, 25);
+            this.txtYcount.TabIndex = 8;
+            this.txtYcount.Text = "21";
+            // 
+            // txtXCount
+            // 
+            this.txtXCount.Location = new System.Drawing.Point(197, 124);
+            this.txtXCount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtXCount.Name = "txtXCount";
+            this.txtXCount.Size = new System.Drawing.Size(132, 25);
+            this.txtXCount.TabIndex = 7;
+            this.txtXCount.Text = "12";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 165);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ycount:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(111, 130);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Xcount:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 195);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 25);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(79, 199);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "SpawnCount:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 310);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtYcount);
+            this.Controls.Add(this.txtXCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtYcount;
+        private System.Windows.Forms.TextBox txtXCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ imageCreater/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace imageCreater
+{
+    public partial class Form1 : Form
+    {
+        private int xCount, yCount, imageWidth, imageHeight,spawnCount,imageCount;
+        private List<string> imageNames = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private List<string> GetImageNamesFormFolder()
+        {
+            string folderPath = Application.StartupPath + "\\gallery";
+            List<string> galleryList = new List<string>();
+            //var galleryList = Directory.GetFiles(folderPath, "*.JPEG").ToList<string>();
+            DirectoryInfo folder = new DirectoryInfo(folderPath);
+
+            foreach (FileInfo file in folder.GetFiles("*.JPEG"))
+            {
+                galleryList.Add(file.Name.Split('.')[0]);
+            }
+            List<string> imageTextList = new List<string>();
+            Random random = new Random();
+            for (int i = 0; i < imageCount; i++)
+            {
+                //判断如果列表还有可以取出的项,以防下标越界
+                if (galleryList.Count > 0)
+                {
+                    //在列表中产生一个随机索引
+                    int arrIndex = random.Next(0, galleryList.Count);
+                    //将此随机索引的对应的列表元素值复制出来
+                    imageTextList.Add(galleryList[arrIndex]);
+                    //然后删掉此索引的列表项
+                    galleryList.RemoveAt(arrIndex);
+                }
+                else
+                {
+                    //列表项取完后,退出循环,比如列表本来只有10项,但要求取出20项.
+                    break;
+                }
+            }
+            return imageTextList;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            xCount = int.Parse(txtXCount.Text);
+            yCount = int.Parse(txtYcount.Text);
+            imageWidth = int.Parse(txtWidth.Text);
+            imageHeight = int.Parse(txtHeight.Text);
+            spawnCount = int.Parse(textBox1.Text);
+            imageCount = xCount * yCount;
+            for (int spawnIndex= 1; spawnIndex <= spawnCount; spawnIndex++)//everytime spawn a image
+            {
+                imageNames = GetImageNamesFormFolder();
+                //Init Graphics,image
+                Graphics resultGraphics;
+                Bitmap resultImg;
+                resultImg = new Bitmap(xCount * imageWidth, yCount * imageHeight);
+                resultGraphics = Graphics.FromImage(resultImg);
+
+                //image info
+                StringBuilder imageInfoBuilder = new StringBuilder();
+                StringBuilder imageLocationBuilder = new StringBuilder();
+
+                for (int imageIndex = 1; imageIndex <= imageCount; imageIndex++)
+                {
+                    var yIndex = Math.Ceiling((double)imageIndex / xCount);
+                    var xIndex = imageIndex % xCount == 0 ? xCount : imageIndex % xCount;
+                    var imageX = (xIndex - 1) * imageWidth;
+                    var imageY = (int)(yIndex - 1) * imageHeight;
+                    string imagePath = string.Format("{0}\\gallery\\{1}.JPEG",Application.StartupPath, imageNames[imageIndex - 1]);
+
+                    //test,print location info
+                    //string imageLocationInfo = string.Format("{0} {1} {2} {3}\n",imageX,imageY,imageWidth,imageHeight);
+                    //imageLocationBuilder.Append(imageLocationInfo);
+                    //image info
+                    string imageName = imageNames[imageIndex - 1].Split('.')[0];
+                    imageInfoBuilder.Append(string.Format("{0},",imageName));
+
+                    DrawImage(resultGraphics, imagePath, imageX, imageY);
+                }
+
+                //Save image
+                resultImg.Save(string.Format("{0}\\image\\{1}.jpg", Application.StartupPath, spawnIndex.ToString()));
+                resultGraphics.Dispose();
+
+                //Save imageInfo
+                string imageInfo = imageInfoBuilder.ToString().TrimEnd(',');
+                string infoPath = string.Format("{0}\\image\\{1}.txt", Application.StartupPath, spawnIndex.ToString());
+                File.WriteAllText(infoPath, imageInfo);
+                //File.WriteAllText(string.Format("{0}\\image\\{1}11.txt", Application.StartupPath, spawnIndex.ToString()), imageLocationBuilder.ToString());
+            }
+        }
+
+        private void DrawImage(Graphics graphics, string imagePath, int imageX, int imageY)
+        {
+            Point leftupConner = new Point(imageX, imageY);
+            Point rightupConner = new Point(imageWidth+imageX,imageY);
+            Point leftfownConner=new Point(imageX, imageY+imageHeight);
+            Point[] locationPoints = new Point[] { leftupConner, rightupConner, leftfownConner };
+            //graphics.DrawImage(Image.FromFile(imagePath), new Point(imageX, imageY));
+            graphics.DrawImage(Image.FromFile(imagePath), locationPoints);
+        }
+    }
+}
